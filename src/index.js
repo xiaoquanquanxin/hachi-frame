2482091a2f58e77e4ea5fcf89234ff2882bec62b
xiaoquanquanxin <xxkl123@126.com>
2019-10-21 15:54:32 +0800

    配置ie兼容，配置windows兼容

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ import router from './route/index';
 // Vue.use(elementUI);
 Vue.use("eui");
 
+//  为ie配置vuex兼容
+import 'babel-polyfill';
+
 // 4. 创建和挂载根实例。
 // 记得要通过 router 配置参数注入路由，从而让整个应用都有路由功能,现在，应用已经启动了！
 
