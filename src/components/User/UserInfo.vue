a8f1a9f67901f4aaa760c9bd7cc423cfec5fc447
xiaoquanquanxin <xxkl123@126.com>
2019-10-23 16:50:52 +0800

    vuex完善

--- src/components/User/UserInfo.vue
+++ src/components/User/UserInfo.vue
@@ -6,6 +6,7 @@
         </el-row>
         <el-row>
             <el-button @click="incrementIfOddOnRootSum(333)">增加333</el-button>
+            <el-button @click="abc(333333)">增加333</el-button>
             <p v-text="getCount"></p>
             <hr>
             <p v-text="doubleVal"></p>
@@ -14,7 +15,7 @@
 </template>
 <script>
     //  工具包了里的方法
-    import {vueMethods, toJSON} from "@aliasAssets/js/utils"
+    import {vueMethods, toJSON} from "@aliasAssets/js/utils";
 
     import {
         //  vuex暴露的mapState，用于处理computed
@@ -42,7 +43,7 @@
         },
         computed: {
             //  计算属性的模块化写法  UserInfoModule是模块命名空间
-            ...mapGetters("UserInfoModule", ['getCount','doubleVal'])
+            ...mapGetters("UserInfoModule", ['getCount', 'doubleVal'])
 
         },
         data: () => ({
@@ -56,6 +57,9 @@
                 vueMethods.goBack.call(this);
             },
             ...mapActions('UserInfoModule', ['incrementIfOddOnRootSum']),
+            abc(params) {
+                console.log(this.$store.dispatch('UserInfoModule/incrementIfOddOnRootSum', params));
+            }
         }
     }
 </script>
--- src/store/UserInfoModule.js
+++ src/store/UserInfoModule.js
@@ -4,20 +4,22 @@ const UserInfoModule = (() => {
     const state = {
         count: 87,
     };
-    // 同步修改状态（同步改变state状态）
+    // 同步修改状态（同步改变state状态）,必须是同步的因为要记录更新的状态，只有真正更新了，才能获得更新后的状态
     const mutations = {
         increment(state, step) {
             // 这里的 `state` 对象是模块的局部状态
             state.count += step || 1;
         }
     };
+    //  们可以在 action 内部执行异步操作
     const actions = {
         incrementIfOddOnRootSum({state, commit, rootState}, step) {
             commit('increment', step);
         }
     };
     const getters = {
-        getCount(state) {
+        getCount(state, getters, rootState) {
+            console.log(state, getters, rootState);
             return state.count;
         },
         doubleVal(state) {
